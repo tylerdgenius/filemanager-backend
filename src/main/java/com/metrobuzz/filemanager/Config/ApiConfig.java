b6ee11c87a7ab17470eba7ffd750cbbdafbadf2f
Chima Nwachukwu <tylerdgenius@Chimas-MBP.(none)>
2023-12-07 18:11:48 +0100

    feat: added swagger to app

---
+++ src/main/java/com/metrobuzz/filemanager/Config/ApiConfig.java
@@ -0,0 +1,10 @@
+package com.metrobuzz.filemanager.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Configuration
+@RequestMapping("/api")
+public class ApiConfig {
+
+}
